33f102b5e95423ad261469d3646e7cd3ee83b68d
S.M. Rafiz <s.m.rafiz@gmail.com>
2023-07-23 00:02:24 +0600

    Updated: Fluent form import system

--- inc/App/Backend/Ajax/ImportFluentForms.php
+++ inc/App/Backend/Ajax/ImportFluentForms.php
@@ -12,8 +12,10 @@ declare( strict_types=1 );
 
 namespace SigmaDevs\EasyDemoImporter\App\Backend\Ajax;
 
+use FluentForm\App\Models\Form;
+use FluentForm\App\Models\FormMeta;
 use FluentForm\Database\DBMigrator;
-use FluentForm\Framework\Helpers\ArrayHelper;
+use FluentForm\Framework\Support\Arr;
 use SigmaDevs\EasyDemoImporter\Common\Abstracts\ImporterAjax;
 use SigmaDevs\EasyDemoImporter\Common\{
 	Traits\Singleton,
@@ -105,37 +107,32 @@ class ImportFluentForms extends ImporterAjax {
 
 			if ( $forms && is_array( $forms ) ) {
 				foreach ( $forms as $formItem ) {
-					// First of all make the form object.
-					$formFields = wp_json_encode( [] );
+					$formFields = json_encode( [] );
 
-					if ( $fields = ArrayHelper::get( $formItem, 'form', '' ) ) {
-						$formFields = wp_json_encode( $fields );
-					} elseif ( $fields = ArrayHelper::get( $formItem, 'form_fields', '' ) ) {
-						$formFields = wp_json_encode( $fields );
-					} else {
+					if ( $fields = Arr::get( $formItem, 'form', '' ) ) {
+						$formFields = json_encode( $fields );
+					} elseif ( $fields = Arr::get( $formItem, 'form_fields', '' ) ) {
+						$formFields = json_encode( $fields );
 					}
 
 					$form = [
-						'title'       => ArrayHelper::get( $formItem, 'title' ),
-						'id'          => ArrayHelper::get( $formItem, 'id' ),
+						'title'       => Arr::get( $formItem, 'title' ),
 						'form_fields' => $formFields,
-						'status'      => ArrayHelper::get( $formItem, 'status', 'published' ),
-						'has_payment' => ArrayHelper::get( $formItem, 'has_payment', 0 ),
-						'type'        => ArrayHelper::get( $formItem, 'type', 'form' ),
+						'status'      => Arr::get( $formItem, 'status', 'published' ),
+						'has_payment' => Arr::get( $formItem, 'has_payment', 0 ),
+						'type'        => Arr::get( $formItem, 'type', 'form' ),
 						'created_by'  => get_current_user_id(),
 					];
 
-					if ( ArrayHelper::get( $formItem, 'conditions' ) ) {
-						$form['conditions'] = ArrayHelper::get( $formItem, 'conditions' );
+					if ( Arr::get( $formItem, 'conditions' ) ) {
+						$form['conditions'] = Arr::get( $formItem, 'conditions' );
 					}
 
 					if ( isset( $formItem['appearance_settings'] ) ) {
-						$form['appearance_settings'] = $formItem['appearance_settings'];
+						$form['appearance_settings'] = Arr::get( $formItem, 'appearance_settings' );
 					}
 
-					// Insert the form to the DB.
-					$formId = wpFluent()->table( 'fluentform_forms' )->insert( $form );
-
+					$formId                   = Form::insertGetId( $form );
 					$insertedForms[ $formId ] = [
 						'title'    => $form['title'],
 						'edit_url' => admin_url( 'admin.php?page=fluent_forms&route=editor&form_id=' . $formId ),
@@ -145,11 +142,11 @@ class ImportFluentForms extends ImporterAjax {
 						foreach ( $formItem['metas'] as $metaData ) {
 							$settings = [
 								'form_id'  => $formId,
-								'meta_key' => $metaData['meta_key'],
-								'value'    => $metaData['value'],
+								'meta_key' => Arr::get( $metaData, 'meta_key' ),
+								'value'    => Arr::get( $metaData, 'value' ),
 							];
 
-							wpFluent()->table( 'fluentform_form_meta' )->insert( $settings );
+							FormMeta::insert( $settings );
 						}
 					} else {
 						$oldKeys = [
@@ -161,13 +158,7 @@ class ImportFluentForms extends ImporterAjax {
 
 						foreach ( $oldKeys as $key ) {
 							if ( isset( $formItem[ $key ] ) ) {
-								$settings = [
-									'form_id'  => $formId,
-									'meta_key' => $key,
-									'value'    => json_encode( $formItem[ $key ] ),
-								];
-
-								wpFluent()->table( 'fluentform_form_meta' )->insert( $settings );
+								FormMeta::persist( $formId, $key, json_encode( Arr::get( $formItem, $key ) ) );
 							}
 						}
 					}
