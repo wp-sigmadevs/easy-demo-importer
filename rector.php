479e65b39300f86c5fb2b0f847d429d5dba420c0
smrafiz <s.m.rafiz@gmail.com>
2026-03-10 10:46:34 +0600

    Removed ABSPATH guard from rector

--- rector.php
+++ rector.php
@@ -8,11 +8,6 @@ declare( strict_types=1 );
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
 
-// Do not allow directly accessing this file.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit( 'This script cannot be accessed directly.' );
-}
-
 return RectorConfig::configure()
 	->withPaths(
 		[
